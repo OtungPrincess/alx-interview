c7e55d5278786fd4229a45003b15ac04a67582b9
Adunni <otungprincess@gmail.com>
2023-01-12 00:28:08 +0000

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Minimum Operations
+"""
+
+
+def factorization(n):
+    """ returns the sum of prime factors of n or n if n is prime"""
+    i = 2
+    s = 0
+    while i <= n:
+        if n % i == 0:
+            n = n // i
+            s += i
+            i -= 1
+        i += 1
+    return s
+
+
+def minOperations(n):
+    """
+    returns the fewest number of needed operations or 0
+    """
+    if not isinstance(n, int) or n < 2:
+        return 0
+    return factorization(n)
